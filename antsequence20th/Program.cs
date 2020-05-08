311a2b417c112c176731a7886c0b0a0db1f017c8
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-08 12:14:48 +0900

    초기 커밋.

---
+++ antsequence20th/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antsequence20th
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string sq = "1";
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine(i+"번  :"+sq);
+                char num = sq[0];
+                int cnt = 0;
+                string temp = " ";
+
+                for (int n = 0; n<sq.Length; n++) {
+                    if (num == sq[n])
+                        cnt++;
+                    else {
+                        temp += "" + num + cnt;
+                        cnt = 0;
+                        num = sq[n];
+                        cnt++;
+                    }
+                    if (n + 1 == sq.Length)
+                        temp += "" + num + cnt;
+                }
+
+                    sq = temp;
+                    i++;
+
+            }
+        }
+    }
+}
